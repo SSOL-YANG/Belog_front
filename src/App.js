3d41f345e6411555f11094bcb04a5a393ca8d1c1
SSOL-YANG <fntldks4@naver.com>
2023-04-26 08:41:08 +0900

    Feat : appjs 파일 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AppRouter from "./router/AppRouter";
 import { Reset } from "styled-reset";
 function App() {
   console.log(
-    "%c DEV Log 포르노젝트입니다.",
+    "%c DEV Log 프로젝트입니다.",
     "color:white; font-size:20px; background: black"
   );
   console.log(
